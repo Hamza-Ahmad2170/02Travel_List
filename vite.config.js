ada3f3cfb6274f113a7133c0f190624291bc89c6
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2024-01-21 06:29:05 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/02Travel_List/"
+  base: "/02Travel_List/",
   plugins: [react()],
 })
